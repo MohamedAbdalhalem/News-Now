aff5b8f5efd1ba80de47ed0ca27e07d7affc395c
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-11-03 16:43:30 +0200

    create the navbar and change the icon , title and description

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Navbar from "@/component/Navbar";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NewsNow",
+  description: "Stay updated with the latest breaking news, trends, analysis, and stories from around the world. Fast, reliable, and unbiased reporting across politics, business, tech, sports, and more.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" >
+      
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
